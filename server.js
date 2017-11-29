9c3a6d331c36d4f7f2faeb1f880c69531338eab2
Daguang Li <Daguang Li>
2017-11-29 15:28:18 +0800

    add server.js used to start production build folder

---
+++ server.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Daguang Li on 11/29/2017.
+ */
+let express = require('express');
+let app = express();
+let path = require('path');
+
+app.use(express.static(path.join(__dirname, './build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+let server = app.listen(8088, () => {
+    let host = server.address().address;
+    let port = server.address().port;
+
+    console.log('server url is: http://%s:%s', host, port);
+});
